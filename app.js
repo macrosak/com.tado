563d11af356ce3ddd19f6e5ae08e27364962d2aa
Emile Nijssen <emile@athom.nl>
2018-01-29 14:56:43 +0100

    fixed bug where refreshed tokens would not be saved

--- app.js
+++ app.js
@@ -9,47 +9,11 @@ class TadoApp extends Homey.App {
 		
 		this.log('TadoApp is running...');
 		
-		/*
+		if( typeof Homey.env.CLIENT_ID === 'undefined' )
+			throw new Error('Missing Client ID. Make sure your env.json is present.');
 		
-		let homeId;
-		let zoneId;
-		let tadoApi = new TadoApi('test@athom.com', 'homeyhomey');
-		tadoApi.login()
-			.then(() => {
-				console.log('Logged in');
-				return tadoApi.getMe();
-			})
-			.then( result => {
-				//console.log(result)
-				homeId = result.homes[0].id;
-				return tadoApi.getZones( homeId );
-			})
-			.then( result => {
-				//console.log(result)
-				zoneId = result[0].id;
-				//return tadoApi.getOverlay( homeId, zoneId )
-			})
-			.then( result => {
-				return tadoApi.setOverlay( homeId, zoneId, {
-					"setting": {
-						"type": "HEATING",
-						"power": "ON",
-						"temperature": {
-							"celsius": 23
-						}
-					},
-					"termination": {
-						"type": "MANUAL"
-					}
-				})
-			})
-			.then(() => {
-				return tadoApi.unsetOverlay( homeId, zoneId );				
-			})
-			.catch( err => {
-				console.error( err );
-			})
-		*/
+		if( typeof Homey.env.CLIENT_SECRET === 'undefined' )
+			throw new Error('Missing Client Secret. Make sure your env.json is present.');
 	}
 	
 }
